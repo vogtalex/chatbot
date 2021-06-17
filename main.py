0147bc75904828458669e7924dfa04a3bfaf93d6
vogtalex <vogtalex@oregonstate.edu>
2021-06-16 21:13:46 -0700

    Tokenized words in patterns and read from intents.json

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data["intents"])
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
