8a531d337ca3b0a48448fa8a9e4adcaca528a7e0
vogtalex <vogtalex@oregonstate.edu>
2021-06-18 15:53:11 -0700

    Formatted code for easier understanding.

--- main.py
+++ main.py
@@ -9,79 +9,7 @@ import random
 import json
 import pickle
 
-with open("intents.json") as file:
-    data = json.load(file)
-
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
-
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
-
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range (len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-
-    training = numpy.array(training)
-    output = numpy.array(output)
-
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
-
-#tensorflow.reset_default_graph()
-
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
-
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=4, show_metric=True)
-    model.save("model.tflearn")
-
-
+#Creates a bag of words using a numpy array. Neccesary for print function 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
@@ -95,8 +23,82 @@ def bag_of_words(s, words):
 
     return numpy.array(bag)
 
+#Prepares data to be read and understood by AI. Reads through json file and makes a one hot encoded array.
+def data_preparation(data):
 
-def chat():
+    global words
+    global labels
+    global docs_x
+    global docs_y
+    global training
+    global output
+
+    try:
+        with open("data.pickle", "rb") as f:
+            words, labels, training, output = pickle.load(f)
+    except:
+
+        for intent in data["intents"]:
+            for pattern in intent["patterns"]:
+                wrds = nltk.word_tokenize(pattern)
+                words.extend(wrds)
+                docs_x.append(wrds)
+                docs_y.append(intent["tag"])
+
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"])
+
+        words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+        words = sorted(list(set(words)))
+
+        labels = sorted(labels)
+
+        out_empty = [0 for _ in range (len(labels))]
+
+        for x, doc in enumerate(docs_x):
+            bag = []
+
+            wrds = [stemmer.stem(w) for w in doc]
+
+            for w in words:
+                if w in wrds:
+                    bag.append(1)
+                else:
+                    bag.append(0)
+
+            output_row = out_empty[:]
+            output_row[labels.index(docs_y[x])] = 1
+
+            training.append(bag)
+            output.append(output_row)
+
+
+        training = numpy.array(training)
+        output = numpy.array(output)
+
+        with open("data.pickle", "wb") as f:
+            pickle.dump((words, labels, training, output), f)
+
+#Trains my model/AI with the data set given
+def model_training(training, output):
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+
+    try:
+        model.load("model.tflearn")
+    except:
+        model.fit(training, output, n_epoch=1000, batch_size=4, show_metric=True)
+        model.save("model.tflearn")
+
+    return model 
+
+#This is the chatting function that allows user to speak with AI
+def chat(model, data):
     print("\n\n\nWelcome. You are ready to begin talking to the bot. Type quit to stop the program.")
     while True:
         inp = input ("\nYou: ")
@@ -117,4 +119,23 @@ def chat():
         else:
             print("Sorry I don't understand your question. Please try again.")
 
-chat()
+
+#Main. Calls other functions and declares basic variables.
+with open("intents.json") as file:
+        data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+training = []
+output = []
+
+
+data_preparation(data)
+
+print(words[0])
+
+model = model_training(training, output)
+
+chat(model, data)
